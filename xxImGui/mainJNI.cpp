df98131dddbd7aa1d7abd505b4f86c9273bb5afb
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2020-03-25 17:34:00 +0800

    TEST2

--- xxImGui/mainJNI.cpp
+++ xxImGui/mainJNI.cpp
@@ -8,7 +8,7 @@
 
 static bool g_initialized = false;
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_create(JNIEnv* env, jobject obj, jobject surface)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_create(JNIEnv* env, jclass obj, jobject surface)
 {
     if (g_initialized)
         return;
@@ -28,12 +28,14 @@ extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_create(JNIEnv* env, jobje
         height = ANativeWindow_getHeight(window);
     }
 
+    xxJNIEnv = env;
+
     Renderer::Create(window, width, height);
     DearImGui::Create(window, 2.0f);
     Plugin::Create("plugin");
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_resize(JNIEnv* env, jobject obj, jint width, jint height, jobject surface)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_resize(JNIEnv* env, jclass obj, jint width, jint height, jobject surface)
 {
     ANativeWindow* window = nullptr;
     if (surface != nullptr)
@@ -44,7 +46,7 @@ extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_resize(JNIEnv* env, jobje
     Renderer::Reset(window, width, height);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_step(JNIEnv* env, jobject obj)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_step(JNIEnv* env, jclass obj)
 {
     DearImGui::NewFrame(Renderer::g_view);
     DearImGui::Update(Plugin::Update() == false);
@@ -65,7 +67,7 @@ extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_step(JNIEnv* env, jobject
     DearImGui::PostUpdate(Renderer::g_view);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_shutdown(JNIEnv* env, jobject obj)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_shutdown(JNIEnv* env, jclass obj)
 {
     Plugin::Shutdown();
     DearImGui::Shutdown();
@@ -74,15 +76,15 @@ extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_shutdown(JNIEnv* env, job
     g_initialized = false;
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_pause(JNIEnv* env, jobject obj)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_pause(JNIEnv* env, jclass obj)
 {
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_resume(JNIEnv* env, jobject obj)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_resume(JNIEnv* env, jclass obj)
 {
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_touch(JNIEnv* env, jobject obj, jint type, jfloat x, jfloat y)
+extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_touch(JNIEnv* env, jclass obj, jint type, jfloat x, jfloat y)
 {
     DearImGui::HandleEventAndroid(type, x, y);
 }
