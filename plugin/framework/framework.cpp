a65aabc7894e6796020dfd47713039bde4712905
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2020-06-05 00:33:10 +0800

    Update projects

--- plugin/framework/framework.cpp
+++ plugin/framework/framework.cpp
@@ -5,7 +5,10 @@
 // https://github.com/metarutaiga/xxImGui
 //==============================================================================
 #include "../../ConcurrencyNetworkFramework/Listen.h"
+#include "../../ConcurrencyNetworkFramework/Log.h"
 #include "../../ConcurrencyNetworkFramework/Framework.h"
+#include <mutex>
+#include <thread>
 #include <interface.h>
 
 #define PLUGIN_NAME     "Framework"
@@ -14,7 +17,7 @@
 
 static Framework* framework = nullptr;
 static Listen* server = nullptr;
-static pthread_t frameworkThread = nullptr;
+static std::thread* frameworkThread = nullptr;
 
 //------------------------------------------------------------------------------
 pluginAPI const char* Create(const CreateData& createData)
@@ -38,7 +41,7 @@ pluginAPI void Update(const UpdateData& updateData)
         {
             ImGui::MenuItem("About " PLUGIN_NAME, nullptr, &showAbout);
             ImGui::Separator();
-            ImGui::MenuItem("Start Framework", nullptr, &showFramework);
+            ImGui::MenuItem("Framework", nullptr, &showFramework);
             ImGui::EndMenu();
         }
         ImGui::EndMainMenuBar();
@@ -57,32 +60,56 @@ pluginAPI void Update(const UpdateData& updateData)
 
     if (showFramework)
     {
-        if (server == nullptr)
-        {
-            server = new Listen("127.0.0.1", "7777");
-        }
-        if (framework == nullptr)
-        {
-            framework = new Framework;
-            framework->Server(server);
-            pthread_create(&frameworkThread, nullptr, [](void*) -> void* { framework->Dispatch(); return nullptr; }, nullptr);
-        }
         if (ImGui::Begin("Framework", &showFramework))
         {
+            static std::string log;
+            static std::mutex logMutex;
+
+            if (ImGui::Button("Start Framework"))
+            {
+                if (server == nullptr || framework == nullptr)
+                {
+                    log.clear();
+                    Log::SetOutput([](const char* output)
+                    {
+                        logMutex.lock();
+                        log += output;
+                        logMutex.unlock();
+                    });
+
+                    server = new Listen("127.0.0.1", "7777");
+                    framework = new Framework;
+                    framework->Server(server);
+                    frameworkThread = new std::thread([]{ framework->Dispatch(); });
+                }
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("Stop Framework"))
+            {
+                if (frameworkThread)
+                {
+                    framework->Terminate();
+                    frameworkThread->join();
+
+                    delete frameworkThread;
+                    delete framework;
+                    delete server;
+
+                    frameworkThread = nullptr;
+                    framework = nullptr;
+                    server = nullptr;
+                }
+            }
+
+            ImVec2 windowSize = ImVec2(320 * updateData.windowScale, 240 * updateData.windowScale);
+
+            logMutex.lock();
+            ImGui::InputTextMultiline("LOG", &log.front(), log.size(), windowSize, ImGuiInputTextFlags_ReadOnly);
+            logMutex.unlock();
+
             ImGui::End();
         }
-    }
-    else if (frameworkThread)
-    {
-        framework->Terminate();
-        pthread_join(frameworkThread, nullptr);
-
-        delete server;
-        delete framework;
-
-        server = nullptr;
-        framework = nullptr;
-        frameworkThread = nullptr;
     }
 }
 //------------------------------------------------------------------------------
