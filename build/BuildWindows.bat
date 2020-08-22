7b1f85e52a7494c3af0273e5a54fbf647ca4241b
TAiGA <@>
2020-08-22 13:52:51 +0800

    Support for Building Windows from Wine

--- build/BuildWindows.bat
+++ build/BuildWindows.bat
@@ -1,5 +1,6 @@
 @echo off
 if "%1" == "" goto option
+if "%1" == "all" goto all
 if not "%1" == "x86" if not "%1" == "x64" if not "%1" == "arm" if not "%1" == "arm64" goto build
 call BuildWindows.bat lib xxMiniCRT %1 %2 %3 %4 %5
 call BuildWindows.bat dll freetype %1 %2 %3 %4 %5
@@ -7,16 +8,29 @@ call BuildWindows.bat dll imgui %1 %2 %3 %4 %5
 call BuildWindows.bat dll xxGraphic %1 %2 %3 %4 %5
 call BuildWindows.bat dll xxGraphicPlus %1 %2 %3 %4 %5
 call BuildWindows.bat exe xxImGui %1 %2 %3 %4 %5
-exit
+call BuildWindows.bat plugin sample %1 %2 %3 %4 %5
+call BuildWindows.bat plugin validator %1 %2 %3 %4 %5
+goto :eof
+
+:all
+call BuildWindows.bat x86 %2 %3 %4 %5 %6
+call BuildWindows.bat x64 %2 %3 %4 %5 %6
+call BuildWindows.bat arm %2 %3 %4 %5 %6
+call BuildWindows.bat arm64 %2 %3 %4 %5 %6
 goto :eof
 
 :option
 echo BuildWindows.bat [Platform] [VisualStudioYear] [VisualStudioEdition] [VisualStudioVersion] [WindowsSDKVersion]
-exit
+echo BuildWindows.bat all 2015
+echo BuildWindows.bat x86 2017 Community 14.13.26128
+echo BuildWindows.bat x64 2019 Community 14.24.28314 10.0.19041.0
+goto :eof
 
 :build
 call vcvars.bat %3 %4 %5 %6 %7
 cd ..
+mkdir bin >nul 2>nul
+mkdir bin\plugin >nul 2>nul
 mkdir lib >nul 2>nul
 mkdir temp >nul 2>nul
 mkdir temp\%2 >nul 2>nul
@@ -27,13 +41,14 @@ cd %3
 cl.exe /MP /nologo /c /O2 /GS- /GR- /Zc:threadSafeInit- /fp:fast @..\..\..\build\%2.sources
 del ..\..\..\bin\%2.Release.%3.lib >nul 2>nul
 set LOCALLIB=..\..\..\bin\*.Release.%3.lib ..\..\..\lib\*.Release.%3.lib
-if "%3" == "x86"   (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.x86.obj)
-if "%3" == "x64"   (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.x64.obj)
-if "%3" == "arm"   (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\arm\*.obj)
-if "%3" == "arm64" (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.arm64.obj)
-if "%1" == "lib"   (lib  /nologo *.obj /OUT:..\..\..\lib\%2.Release.%3.lib)
-if "%1" == "dll"   (link /nologo *.obj /OUT:..\..\..\bin\%2.Release.%3.dll %LOCALLIB% kernel32.lib user32.lib /dll)
-if "%1" == "exe"   (link /nologo *.obj /OUT:..\..\..\bin\%2.Release.%3.exe %LOCALLIB% kernel32.lib user32.lib /LIBPATH:..\..\..\lib)
+if "%3" == "x86"    (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.x86.obj)
+if "%3" == "x64"    (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.x64.obj)
+if "%3" == "arm"    (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\arm\*.obj)
+if "%3" == "arm64"  (set LOCALLIB=%LOCALLIB% ..\..\..\xxMiniCRT\chkstk.arm64.obj)
+if "%1" == "lib"    (lib  /nologo *.obj /OUT:..\..\..\lib\%2.Release.%3.lib)
+if "%1" == "dll"    (link /nologo *.obj /OUT:..\..\..\bin\%2.Release.%3.dll %LOCALLIB% kernel32.lib user32.lib /dll)
+if "%1" == "exe"    (link /nologo *.obj /OUT:..\..\..\bin\%2.Release.%3.exe %LOCALLIB% kernel32.lib user32.lib /LIBPATH:..\..\..\lib)
+if "%1" == "plugin" (link /nologo *.obj /OUT:..\..\..\bin\plugin\%2.Release.%3.dll %LOCALLIB% kernel32.lib user32.lib /dll)
 cd ..
 cd ..
 cd ..
