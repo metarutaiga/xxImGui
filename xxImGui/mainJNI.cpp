6f66be243da35c17006f498e278d6ee86f7eed92
TAiGA <@>
2020-10-22 11:05:59 +0800

    Update

--- xxImGui/mainJNI.cpp
+++ xxImGui/mainJNI.cpp
@@ -1,6 +1,5 @@
 #include <android/native_window_jni.h>
 #include <jni.h>
-#include <locale.h>
 
 #include "Renderer.h"
 #include "Plugin.h"
@@ -14,8 +13,6 @@ extern "C" JNIEXPORT void JNICALL Java_com_xx_Activity_create(JNIEnv* env, jclas
     if (g_initialized)
         return;
     g_initialized = true;
-
-    setlocale(LC_ALL, "ja_JP.UTF-8");
 
     ANativeWindow* window = nullptr;
     if (surface != nullptr)
