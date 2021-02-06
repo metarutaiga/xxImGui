42e7b59a280930054c02e33017f4531aa4ac2916
TAiGA <@>
2021-02-07 01:29:09 +0800

    Add compiler versions

--- plugin/sample/sample.cpp
+++ plugin/sample/sample.cpp
@@ -54,6 +54,15 @@ pluginAPI bool Update(const UpdateData& updateData)
             ImGui::Text("%s Plugin Version %d.%d", PLUGIN_NAME, PLUGIN_MAJOR, PLUGIN_MINOR);
             ImGui::Separator();
             ImGui::Text("Build Date : %s %s", __DATE__, __TIME__);
+#if defined(__clang_version__)
+            ImGui::Text("Compiler Version : %s", __clang_version__);
+#endif
+#if defined(__GNUC__)
+            ImGui::Text("Compiler Version : %d.%d.%d", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+#endif
+#if defined(_MSC_FULL_VER)
+            ImGui::Text("Compiler Version : %d.%d.%d", _MSC_FULL_VER / 10000000 % 100, _MSC_FULL_VER / 100000 % 100, _MSC_FULL_VER % 100000);
+#endif
             ImGui::End();
         }
     }
