57d690c37162d4b0c5cfb821a985529c737feb7a
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2021-08-25 15:18:59 +0800

    Update

---
+++ xxImGui/implement/ftoption.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+ *
+ * ftoption.h
+ *
+ *   User-selectable configuration macros (specification only).
+ *
+ * Copyright (C) 1996-2021 by
+ * David Turner, Robert Wilhelm, and Werner Lemberg.
+ *
+ * This file is part of the FreeType project, and may only be used,
+ * modified, and distributed under the terms of the FreeType project
+ * license, LICENSE.TXT.  By continuing to use, modify, or distribute
+ * this file you indicate that you have read the license and
+ * understand and accept it fully.
+ *
+ */
+
+#include <freetype/config/ftoption.h>
+
+#undef FT_CONFIG_OPTION_USE_LZW
+#undef FT_CONFIG_OPTION_USE_ZLIB
+
+/* END */
